df62c99fe1e53b52927aec15d678e516d45c5543
Bart Voigt <garashon@gmail.com>
2024-04-07 16:02:32 -0400

    Initial commit of locally developed parser

---
+++ App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using Windows.UI.Notifications;
+//using AuthenticationServices;
+//using Microsoft.Maui.Controls.Compatibility.Platform.iOS;
+//using Microsoft.CodeAnalysis.CSharp.Scripting;
+namespace WrathBlueprintTree;
+
+public partial class App : Application
+{
+	public App()
+	//public App(MainPage page)
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+		//MainPage = page;
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace WrathBlueprintTree;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+
+		var bpObject = new BlueprintObject(editor.Text);
+	}
+
+	void OnEditorTextChanged(object sender, TextChangedEventArgs e)
+	{
+		string oldText = e.OldTextValue;
+		string newText = e.NewTextValue;
+		string currentText = editor.Text;
+		Console.WriteLine(currentText.Length);
+	}
+	
+}
+
