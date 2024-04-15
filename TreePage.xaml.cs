987c08c4510f6f162148d3ecb3aae57f381cada7
bvoigt <garashon@gmail.com>
2024-04-14 22:32:51 -0400

    Added AbsoluteLayout to TreeScroll view, discovers drop Point{}, generates Frame object at drop location with sample non-packaged data

--- TreePage.xaml.cs
+++ TreePage.xaml.cs
@@ -36,46 +36,60 @@ public partial class TreePage : ContentPage
         }   
 	}
 
-	public void OnDropBpOption(object sender, DropEventArgs e)
+	Point? relativeToContainerPosition;
+	public void OnPointerMovedInTreeView(object sender, PointerEventArgs e)
+	{
+		relativeToContainerPosition = e.GetPosition((View)sender);
+		Console.WriteLine(relativeToContainerPosition);
+	}
+
+	public void OnDragHandler(object sender, DragEventArgs e)
+	{
+		relativeToContainerPosition = e.GetPosition((View)sender);
+		Console.WriteLine(relativeToContainerPosition);
+
+	}
+	public void OnDropIntoTreeLayout(object sender, DropEventArgs e)
 	{
 		Console.WriteLine(sender.ToString());
 		Console.WriteLine(e.ToString());
-
+		Console.WriteLine((sender as Element)?.Parent as AbsoluteLayout);
+		AbsoluteLayout senderParent = (sender as Element)?.Parent as AbsoluteLayout;
+		//Console.WriteLine((sender);
+		//relativeToContainerPosition = e.GetPosition(treeScrollView);
+		relativeToContainerPosition = e.GetPosition((sender as Element)?.Parent);
+		//if (relativeToContainerPosition!=null){
+			Console.WriteLine($"X = {relativeToContainerPosition.Value.X}");
+			Console.WriteLine($"Y = {relativeToContainerPosition.Value.Y}");
+		//}
+		placeNewBpTemplateFrame(senderParent, relativeToContainerPosition, "TestData Keyed");
 		// Perform logic to take action based on retrieved value.
 	}
 
-	/*public void MenuBarFileOpen(object sender, System.EventArgs e)
-	//async void MenuTesting(object sender, System.EventArgs e)
+	public void placeNewBpTemplateFrame(AbsoluteLayout targetParent, Point? dropPosition, string dropData)
 	{
-		//OpenFile_Bp(sender, e);
-		var (RawFileText, fileFullPath) = BpFile.Open(sender, e);
-		editor.Text = RawFileText;
-		bpFilePath.Text = fileFullPath;
-		bpObject = new BlueprintObject(RawFileText);
-		if (bpObject is WrathBlueprintTree.BlueprintObject){
-			bpAvailable = true;
-		}
-		//await DisplayAlert("Alert", "You have been alerted", "OK");
-	}
+		Frame newBpFrame = new Frame{BackgroundColor = Color.FromRgba("#EEEEEEFF"),
+										Margin = new Thickness(0,2),
+										Padding = new Thickness(0,0),
+										CornerRadius = 3,
+										//WidthRequest= 200,
+										//HeightRequest = 400,
+										//VerticalOptions = LayoutOptions.Top,
+										//HorizontalOptions = LayoutOptions.Left
+									};
+		//newBpFrame.AbsoluteLayout.LayoutBoundsProperty=(5,5,50,50);
 
-	void OnEditorTextChanged(object sender, TextChangedEventArgs e)
-	{
-		string oldText = e.OldTextValue;
-		string newText = e.NewTextValue;
-		string currentText = editor.Text;
-		Console.WriteLine(currentText.Length);
+		Label newBpframeLabel = new Label{Text = $"Data text: {dropData}", 
+										FontSize = 12, 
+										Margin =  new Thickness(0,0), 
+										HorizontalTextAlignment = TextAlignment.Center,
+										VerticalTextAlignment = TextAlignment.Center
+									};
+		newBpFrame.Content = newBpframeLabel;
+		AbsoluteLayout.SetLayoutBounds(newBpFrame, new Rect(dropPosition.Value.X,dropPosition.Value.Y,200,400));
+        //AbsoluteLayout.SetLayoutFlags(newBpFrame, AbsoluteLayoutFlags.PositionProportional);
+		//targetParent.Add(newBpFrame, new Rect(dropPosition.Value.X,dropPosition.Value.Y,200,400));
+		targetParent.Add(newBpFrame);
 	}
-	
-	public void RunBpDotTrace(object sender, TextChangedEventArgs e)
-	{
-		string text = ((Entry)sender).Text;
-		//Console.WriteLine(text);
-		//Console.WriteLine("bpObject = "+bpObject);
-		if (bpAvailable){
-			Console.WriteLine(text);
-		}
-		//ToDo - converts bpData dot syntax result to text/string and dumps it into a multi-line text box (editor?) 
-	}
-    */
 }
 
