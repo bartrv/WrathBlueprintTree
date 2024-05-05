cf6ce29452236da46939834f783764e7f1b77eb4
bvoigt <garashon@gmail.com>
2024-05-05 17:30:31 -0400

    Moved GenerateTreeFromFileData from TreeView.Xaml.cs to TreeBuilder.cs, and added preliminary panel generation for opened json files.

--- TreeBuilder.cs
+++ TreeBuilder.cs
@@ -1,11 +1,237 @@
 using Microsoft.Extensions.Logging;
-
+using System.Collections;
+using System.Collections.Specialized;
 namespace WrathBlueprintTree;
 
 public partial class TreeBuilder
 {
-    public static bool GenerateTreeFromFileData()
+	IDataTransfer XferObject = DependencyService.Get<IDataTransfer>();
+	//AbsoluteLayout targetParent;
+    public static bool GenerateTreeFromFileData(Dictionary<string,dynamic> xferObject, AbsoluteLayout? targetParent = null)
     {
+		Frame newGenPanel;
+		int panelHeight;
+		int panelWidth;
+        Point dropPosition = new() { X = 5, Y = 5 };
+		if (targetParent != null)
+		{
+			//targetParent = WrathBlueprintTree.TreePage.TreeDropContainer;
+			foreach (var (branchName, branchStructure) in xferObject)
+			{
+				//(newGenPanel, panelHeight, panelWidth) = TreePage.GenerateNewBpTemplatePanelFromListModel(PanelBpUniqueName);
+				(newGenPanel, panelHeight, panelWidth) = GenerateNewBpTemplatePanelFromListModel(branchName);
+
+				AbsoluteLayout.SetLayoutBounds(newGenPanel, new Rect(dropPosition.X,dropPosition.Y,panelWidth+16,panelHeight)); //Assign relative location and size data to panel object
+				targetParent.Add(newGenPanel); // Assign panel to Frame
+				dropPosition.X = dropPosition.X+panelWidth+16+50;
+			}
+			return true;
+		}
         return false;
     }
+
+    public Frame GenerateFrameFromData(string bpModelKey, List<dynamic> PanelData)
+	{
+		//var dropData = "testData";
+		int testWidth = 200;
+		int testHeight = 400;
+		Frame bpFrameContainer = new Frame{BackgroundColor = Color.FromRgba("#EEEEEEFF"), //Overall Frame container
+										Margin = new Thickness(0,2),
+										Padding = new Thickness(0,0),
+										CornerRadius = 0
+										};
+		AbsoluteLayout bpFrameContainerAbsLayout = [];
+		
+		BoxView bpNode = new BoxView{BackgroundColor = Color.FromRgba("#BBBBBBFF"),
+						Margin = new Thickness (0,0),
+						//WidthRequest = 16,
+						//HeightRequest = 16,
+						CornerRadius = 8
+						};
+		AbsoluteLayout.SetLayoutBounds(bpNode, new Rect(testWidth-20,28,16,16));
+
+		Frame bpFrameVisualMain = new Frame{BackgroundColor = Color.FromRgba("#EEEEEEFF"), //Visual Frame container
+										Margin = new Thickness(0,2),
+										Padding = new Thickness(0,0),
+										CornerRadius = 3
+										};
+		AbsoluteLayout.SetLayoutBounds(bpFrameVisualMain, new Rect(10,0,testWidth-20,testHeight-4));
+
+		VerticalStackLayout bpFrameVisualMainVertLayout = [];
+
+		
+		Label bpframeLabel = new Label{Text = $": {BlueprintModels.ModelDictionary[bpModelKey]["buttonName"]} :", 
+										FontSize = 12, 
+										Margin =  new Thickness(0,0), 
+										HorizontalTextAlignment = TextAlignment.Center,
+										VerticalTextAlignment = TextAlignment.Start
+									};
+		Label bpframeLineItem = new Label{Text = $"$type: {BlueprintModels.ModelDictionary[bpModelKey]["$type"]}", 
+										FontSize = 10, 
+										Margin =  new Thickness(0,0), 
+										HorizontalTextAlignment = TextAlignment.Start,
+										VerticalTextAlignment = TextAlignment.Start
+									};
+		bpFrameVisualMainVertLayout.Add(bpframeLabel);		
+		bpFrameVisualMainVertLayout.Add(bpframeLineItem);
+		bpFrameVisualMain.Content = bpFrameVisualMainVertLayout;
+
+		bpFrameContainerAbsLayout.Add(bpFrameVisualMain);
+		bpFrameContainerAbsLayout.Add(bpNode);
+		bpFrameContainer.Content = bpFrameContainerAbsLayout;
+		return bpFrameContainer;
+		
+	}
+	
+	public static (Frame, int, int) GenerateNewBpTemplatePanelFromListModel(string PanelBpUniqueName)
+	{
+		IDataTransfer XferObject = DependencyService.Get<IDataTransfer>();
+		int panelWidth = 300;
+		int panelHeight = 36;
+		//Entry TempVMEntryForLoop;
+
+		Frame bpFrameContainer = new Frame{BackgroundColor = Color.FromRgba("#EEEEEEFF"), //Overall Frame container
+										Margin = new Thickness(0,2),
+										Padding = new Thickness(0,0),
+										CornerRadius = 0
+										};
+		AbsoluteLayout bpFrameContainerAbsLayout = [];
+		
+		
+		/*BoxView bpNode = new BoxView{BackgroundColor = Color.FromRgba("#BBBBBBFF"),
+						Margin = new Thickness (0,0),
+						CornerRadius = 8,
+						};
+		AbsoluteLayout.SetLayoutBounds(bpNode, new Rect(panelWidth-20,28,16,16));
+		*/
+		Frame bpFrameVisualMain = new Frame{BackgroundColor = Color.FromRgba("#EEEEEEFF"), //Visual Frame container
+										Margin = new Thickness(0,2),
+										Padding = new Thickness(0,0),
+										CornerRadius = 3
+										};
+	
+		VerticalStackLayout bpFrameVisualMainVertLayout = [];
+		// XferObject.FullBpTree.tree[PanelBpUniqueName].Add(tempList);
+		//[0] = ["Categories", ..], [1] = ["buttonName", .. ], [2] = ["title", .. ]
+		Console.WriteLine("buttonName"+XferObject.FullBpTree.tree[PanelBpUniqueName]["buttonName"][0]); //Static issues, works when parent method is not static
+		/*
+		Label bpframeBtnName = new Label{Text = $": {XferObject.FullBpTree.tree[PanelBpUniqueName]["buttonName"][0]} :", 
+										FontSize = 12, 
+										Margin =  new Thickness(0,0), 
+										HorizontalTextAlignment = TextAlignment.Center,
+										VerticalTextAlignment = TextAlignment.Start
+									};
+		Label bpframeTitle = new Label{Text = $"{XferObject.FullBpTree.tree[PanelBpUniqueName]["title"][4]}", 
+										FontSize = 10, 
+										Margin =  new Thickness(0,0), 
+										HorizontalTextAlignment = TextAlignment.Start,
+										VerticalTextAlignment = TextAlignment.Start
+									};
+		
+		bpFrameVisualMainVertLayout.Add(bpframeBtnName);
+		bpFrameVisualMainVertLayout.Add(bpframeTitle);	
+		*/
+		int ItemCount = XferObject.FullBpTree.tree[PanelBpUniqueName].Count;
+		Grid panelLayoutGrid = new Grid{
+			 RowDefinitions = new RowDefinitionCollection()
+		};
+		RowDefinition rowDef = new RowDefinition{ Height = new GridLength(18, GridUnitType.Absolute) }; //18 = Node rect height + 2
+		//rowDef.Height = 12;
+		
+		ColumnDefinition ColOne = new ColumnDefinition();
+		ColumnDefinition ColTwo = new ColumnDefinition();
+		ColumnDefinition ColThree = new ColumnDefinition();
+		ColOne.Width = 180;
+		ColTwo.Width = 100;
+		ColThree.Width = 20;
+		
+		for (int i = 2; i < ItemCount; i++)
+		{
+			panelLayoutGrid.RowDefinitions.Add(rowDef);
+		}
+		panelLayoutGrid.ColumnDefinitions.Add(ColOne);
+		panelLayoutGrid.ColumnDefinitions.Add(ColTwo);
+		panelLayoutGrid.ColumnDefinitions.Add(ColThree);
+
+		int PanelLayoutGridRow = 0;
+		OrderedDictionary EntryListItem = XferObject.FullBpTree.tree[PanelBpUniqueName];
+		IDictionaryEnumerator EntryListEnum = EntryListItem.GetEnumerator();
+		EntryListEnum.MoveNext();
+		EntryListEnum.MoveNext();
+		EntryListEnum.MoveNext();
+		//for (int i = 3; i < ItemCount; i++)
+		while (EntryListEnum.MoveNext())
+		{
+			//List<string> AnotherCopyIdx = EntryListItem[i] as List<string>;
+			//string EntryItemKey = EntryListItem[i].Last();
+			List<dynamic> AnotherCopyIdx = new() {"0"};
+			//List<dynamic> AnotherCopyIdxB;
+			string EntryItemKey;
+			if (XferObject.IsFile==true)
+			{
+				//var AnotherCopyIdxb = EntryListEnum.Value as List<dynamic>;
+				AnotherCopyIdx = EntryListEnum.Value as List<dynamic>;
+				//string Test0 = AnotherCopyIdxB[0];
+				//string Test1 = AnotherCopyIdxB[1];
+				EntryItemKey = EntryListEnum.Key as string;
+			} else {
+				AnotherCopyIdx = EntryListEnum.Value as List<dynamic>;
+				EntryItemKey = EntryListEnum.Key as string;
+			}
+			
+			//Console.WriteLine("AnotherCopyIdx[0]:"+AnotherCopyIdx[0]);
+			Console.WriteLine("EntryItemKey:"+EntryItemKey);
+			Console.WriteLine("AnotherCopyIdx:"+AnotherCopyIdx);
+			BoxView bpNode = new BoxView{};
+			var TempVMLabelForLoop = new Label{
+											Text = EntryItemKey,
+											//Text = AnotherCopyIdx.Last(),
+											FontSize = 10, 
+											Margin =  new Thickness(0,0), 
+											HorizontalTextAlignment = TextAlignment.Start,
+											VerticalTextAlignment = TextAlignment.Start
+											};
+			
+			Grid.SetRow(TempVMLabelForLoop,PanelLayoutGridRow);
+			Grid.SetColumn(TempVMLabelForLoop,0);
+			panelLayoutGrid.Children.Add(TempVMLabelForLoop);
+
+			if (XferObject.IsFile != true){
+				if (AnotherCopyIdx[2] == "node"){
+					//BoxView bpNode = new BoxView{
+							bpNode.BackgroundColor = Color.FromRgba("#CCBBBBFF");
+							bpNode.Margin = new Thickness (0,0);
+							bpNode.WidthRequest = 16;
+							bpNode.HeightRequest = 16;
+							bpNode.CornerRadius = 8;
+							bpNode.ZIndex = 100;
+							//bpNode.VerticalOptions = Rect;
+							//};
+					//AbsoluteLayout.SetLayoutBounds(bpNode, new Rect(panelWidth-20,panelHeight-17,16,16));
+					//Grid.SetRow(bpNode,PanelLayoutGridRow);
+					//Grid.SetColumn(bpNode,2);
+					//panelLayoutGrid.Children.Add(bpNode);
+				}
+
+				AbsoluteLayout.SetLayoutBounds(bpNode, new Rect(panelWidth-18,panelHeight-17,16,16));
+				 if (AnotherCopyIdx[2] == "node"){bpFrameContainerAbsLayout.Add(bpNode);}
+			} else {
+				//DoDo Add Node identifier to tree data for ingested files
+			}
+			PanelLayoutGridRow++;
+			panelHeight = panelHeight+18;
+			
+		}
+		AbsoluteLayout.SetLayoutBounds(bpFrameVisualMain, new Rect(10,0,panelWidth-20, panelHeight-4)); //Set width, height of Visual Container based on Generated Content
+
+		bpFrameVisualMainVertLayout.Add(panelLayoutGrid);
+		bpFrameVisualMain.Content = bpFrameVisualMainVertLayout;
+
+		bpFrameContainerAbsLayout.Add(bpFrameVisualMain);
+		//bpFrameContainerAbsLayout.Add(bpNode);
+		bpFrameContainer.Content = bpFrameContainerAbsLayout;
+
+		return (bpFrameContainer, panelHeight, panelWidth);
+	}
+
 }
--- TreePage.xaml.cs
+++ TreePage.xaml.cs
@@ -1,10 +1,9 @@
-﻿using System.ComponentModel;
+﻿using System.Collections;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Windows.Input;
-//using Foundation;
-using Microsoft.Extensions.Options;
-//using Microsoft.UI.Xaml.Controls.Primitives;
-//using Microsoft.UI.Windowing;
-//using Microsoft.UI.Xaml;
+using Microsoft.Maui.Controls;
+using Windows.UI.Notifications;
 namespace WrathBlueprintTree;
 
 public partial class TreePage : ContentPage
@@ -12,6 +11,7 @@ public partial class TreePage : ContentPage
 	IDataTransfer XferObject = DependencyService.Get<IDataTransfer>();
 	Dictionary<string,string> ButtonList = [];
 	Point? relativeToContainerPosition;
+    Dictionary<string,dynamic> TreeViewMVVM = [];
 	public TreePage()
 	{
 		InitializeComponent();
@@ -21,16 +21,24 @@ public partial class TreePage : ContentPage
 		generateTreePageButtonList(); //Build the button list - Currently everything in the model(W.i.P), will need sub categories eventually
 		if (XferObject.IsFile == true) 
 		{
-			if (!TreeBuilder.GenerateTreeFromFileData()) 
+			//if (!TreeBuilder.GenerateTreeFromFileData(XferObject.IngestedBpObjectFlat)) 
+			if (!TreeBuilder.GenerateTreeFromFileData(XferObject.FullBpTree.tree, this.TreeDropContainer)) 
 			{
 				ThrowAlert();
+			} else {
+
 			}
+
+		} else {
+			XferObject.FullBpTree = new FullBpTreeCollection(null);
+			XferObject.IsNew = true;
+			XferObject.IsSaved = false;
 		}
 	}
 
 	async public static void ThrowAlert()
 	{
-			bool x =  await Application.Current.MainPage.DisplayAlert("Tittle","Hello","OK","NotOK");
+			bool x =  await Application.Current.MainPage.DisplayAlert("Title","Hello","OK","NotOK");
 	}
 	private void generateTreePageButtonList()
 	{
@@ -46,14 +54,23 @@ public partial class TreePage : ContentPage
 										VerticalOptions = LayoutOptions.Center,
 										HorizontalOptions = LayoutOptions.Center
 									};
-            
-			Label frameLabel = new Label{Text = entry.Value["buttonName"], 
-										FontSize = 12, 
-										Margin =  new Thickness(0,0), 
-										HorizontalTextAlignment = TextAlignment.Center,
-										VerticalTextAlignment = TextAlignment.Center
-									};
-			
+            Label frameLabel;
+			Type testingType = entry.Value["buttonName"].GetType();
+			if (testingType == typeof(List<string>)){
+				frameLabel = new Label{Text = entry.Value["buttonName"][0], 
+											FontSize = 12, 
+											Margin =  new Thickness(0,0), 
+											HorizontalTextAlignment = TextAlignment.Center,
+											VerticalTextAlignment = TextAlignment.Center
+										};
+			} else {
+				frameLabel = new Label{Text = entry.Value["buttonName"], 
+											FontSize = 12, 
+											Margin =  new Thickness(0,0), 
+											HorizontalTextAlignment = TextAlignment.Center,
+											VerticalTextAlignment = TextAlignment.Center
+										};
+			}
 			newFrame.Content = frameLabel;
 
 			DragGestureRecognizer DragFrame = new()
@@ -87,39 +104,274 @@ public partial class TreePage : ContentPage
 		Console.WriteLine(e.ToString());
 		Console.WriteLine($"bpModelKey: {bpModelKey}");
 		object bpModelReferanceObject;
-		if (BlueprintModels.checkBpModel(bpModelKey, out bpModelReferanceObject)){
-			Dictionary<string,dynamic> bpModelDictionary = (Dictionary<string,dynamic>)bpModelReferanceObject;
+		if (BlueprintModels.GetBpModel(bpModelKey, out bpModelReferanceObject)) //verify model data exists in model library and extract lib referance before generating panel
+		{ 
 
-			Console.WriteLine($"bpModel $type: {bpModelDictionary["$type"]}");
-			Console.WriteLine((sender as Element)?.Parent as AbsoluteLayout);
-			AbsoluteLayout? senderParent = (sender as Element)?.Parent as AbsoluteLayout;
+			AbsoluteLayout? senderParent = (sender as Element)?.Parent as AbsoluteLayout; // grab Frame layout object that received drop and re-assert the coordinate system
+			Console.WriteLine(senderParent.ToString());
 			//Console.WriteLine((sender);
-			//relativeToContainerPosition = e.GetPosition(treeScrollView);
-			relativeToContainerPosition = e.GetPosition((sender as Element)?.Parent);
+			relativeToContainerPosition = e.GetPosition((sender as Element)?.Parent); // grab the drop location from drop event args
 			//if (relativeToContainerPosition!=null){
-				Console.WriteLine($"X = {relativeToContainerPosition.Value.X}");
-				Console.WriteLine($"Y = {relativeToContainerPosition.Value.Y}");
+				//Console.WriteLine($"X = {relativeToContainerPosition.Value.X}");
+				//Console.WriteLine($"Y = {relativeToContainerPosition.Value.Y}");
 			//}
-			placeNewBpTemplateFrame(senderParent, relativeToContainerPosition, bpModelKey);
+			placeNewBpTemplatePanel(senderParent, relativeToContainerPosition, bpModelKey); //call new panel generation method and attach it to the parent Frame at the Drop location
 		} else {
 			Console.WriteLine("Exception!! BlueprintModels Dictionaty key not found, this should be immpossible. TreePage.OnDropIntoTreeLayout");
 		}
 		// Perform logic to take action based on retrieved value.
 	}
 
-	public void placeNewBpTemplateFrame(AbsoluteLayout targetParent, Point? dropPosition, string bpModelKey)
+	public void placeNewBpTemplatePanel(AbsoluteLayout targetParent, Point? dropPosition, string bpModelKey)
 	{
-		Frame newGenFrame = GenerateNewBpTemplateFrame(bpModelKey);
-		AbsoluteLayout.SetLayoutBounds(newGenFrame, new Rect(dropPosition.Value.X,dropPosition.Value.Y,200,400));
-		//Todo Generate new class to hold data object
-		targetParent.Add(newGenFrame);
+		Frame newGenPanel;
+		string PanelBpUniqueName;
+		int panelHeight;
+		int panelWidth;
+		if (bpModelKey == "BlueprintProgression") {
+			PanelBpUniqueName = BpTreeFlatFileAddTo_PreVM(bpModelKey);
+			BpTreeVmAddTo(PanelBpUniqueName, dropPosition);
+			//(PanelBpName, newGenPanel) = GenerateNewBpTemplatePanelFromListModel(bpModelKey); //Generate Panel Object, this is backwards... Should Add/populate FlatFileData 1st, then generate VM, THEN VMMV
+			(newGenPanel, panelHeight, panelWidth) = GenerateNewBpTemplatePanelFromListModel(PanelBpUniqueName);
+
+			AbsoluteLayout.SetLayoutBounds(newGenPanel, new Rect(dropPosition.Value.X,dropPosition.Value.Y,panelWidth+16,panelHeight)); //Assign relative location and size data to panel object
+			targetParent.Add(newGenPanel); // Assign panel to Frame
+		} else{
+			(PanelBpUniqueName, newGenPanel) = GenerateNewBpTemplatePanel(bpModelKey); //Generate Panel Object
+			AbsoluteLayout.SetLayoutBounds(newGenPanel, new Rect(dropPosition.Value.X,dropPosition.Value.Y,200,400)); //Assign relative location and size data to panel object
+			targetParent.Add(newGenPanel); // Assign panel to Frame
+			BpTreeVmAddTo(PanelBpUniqueName, dropPosition);
+			BpTreeFlatFileAddTo(PanelBpUniqueName, bpModelKey);
+		}
+
 	}
 
-	public Frame GenerateNewBpTemplateFrame(string bpModelKey)
+	private void BpTreeVmAddTo(string PanelBpUniqueName, Point? dropPosition)
+	{
+		XferObject.FullBpTree.BpTreeVM["Panels"].Add(PanelBpUniqueName, new Dictionary<string,dynamic>{});
+		XferObject.FullBpTree.BpTreeVM["Panels"][PanelBpUniqueName].Add("Location",dropPosition);
+		XferObject.FullBpTree.BpTreeVM["Panels"][PanelBpUniqueName].Add("Wires",new Dictionary<string,string>{});
+	}
+
+	private string BpTreeFlatFileAddTo_PreVM(string bpModelKey)
+	{
+		object bpModelReferanceObject;
+		string PanelBpNameBase = BlueprintModels.ModelDictionary[bpModelKey]["$type"][4].Split(',')[1];
+		string PanelBpUniqueName = $"${PanelBpNameBase}${Guid.NewGuid()}";
+
+		if (BlueprintModels.GetBpModel(bpModelKey, out bpModelReferanceObject)) //verify model data exists in model library and extract lib referance before generating panel
+		{ 
+			Type testingType = bpModelReferanceObject.GetType();
+			if (testingType == typeof(Dictionary<string,dynamic>)){
+				Dictionary<string,dynamic> bpModelDictionary = (Dictionary<string,dynamic>)bpModelReferanceObject;
+				
+				XferObject.FullBpTree.tree.Add(PanelBpUniqueName, new List<dynamic>{});
+				foreach (string modelItemKey in bpModelDictionary.Keys)
+				{
+					Console.WriteLine(modelItemKey);
+					Console.WriteLine(bpModelDictionary[modelItemKey]);
+					XferObject.FullBpTree.tree[PanelBpUniqueName].Add(new List<dynamic>{modelItemKey,""});
+				}
+			} else {
+				OrderedDictionary bpModelDictionary = (OrderedDictionary)bpModelReferanceObject;
+				
+				//XferObject.FullBpTree.tree.Add(PanelBpUniqueName, new List<string>{});
+				XferObject.FullBpTree.tree.Add(PanelBpUniqueName, new OrderedDictionary{});
+				List<string> tempList;
+				foreach (string modelItemKey in bpModelDictionary.Keys)
+				{
+					Console.WriteLine(modelItemKey);
+					Console.WriteLine(bpModelDictionary[modelItemKey]);
+					//tempList = new List<string>{modelItemKey};
+					//tempList.AddRange((List<string>)bpModelDictionary[modelItemKey]);
+					//tempList = [modelItemKey, .. (List<string>)bpModelDictionary[modelItemKey]];
+					XferObject.FullBpTree.tree[PanelBpUniqueName].Add(modelItemKey,bpModelDictionary[modelItemKey]);
+				}
+			}
+
+		}
+		
+		return PanelBpUniqueName;
+
+	}
+
+	private void BpTreeFlatFileAddTo(string PanelBpName, string bpModelKey)
+	{
+		object bpModelReferanceObject;
+		
+		if (BlueprintModels.GetBpModel(bpModelKey, out bpModelReferanceObject)) //verify model data exists in model library and extract lib referance before generating panel
+		{ 
+			Type testingType = bpModelReferanceObject.GetType();
+			if (testingType == typeof(Dictionary<string,dynamic>)){
+				Dictionary<string,dynamic> bpModelDictionary = (Dictionary<string,dynamic>)bpModelReferanceObject;
+				
+				XferObject.FullBpTree.tree.Add(PanelBpName, new List<dynamic>{});
+				foreach (string modelItemKey in bpModelDictionary.Keys)
+				{
+					Console.WriteLine(modelItemKey);
+					Console.WriteLine(bpModelDictionary[modelItemKey]);
+					XferObject.FullBpTree.tree[PanelBpName].Add(new List<dynamic>{modelItemKey,""});
+				}
+			} else {
+				Dictionary<string,List<string>> bpModelDictionary = (Dictionary<string,List<string>>)bpModelReferanceObject;
+				
+				XferObject.FullBpTree.tree.Add(PanelBpName, new List<string>{});
+				List<string> tempList;
+				foreach (string modelItemKey in bpModelDictionary.Keys)
+				{
+					Console.WriteLine(modelItemKey);
+					Console.WriteLine(bpModelDictionary[modelItemKey]);
+					//tempList = new List<string>{modelItemKey};
+					//tempList.AddRange(bpModelDictionary[modelItemKey]);
+					tempList = [modelItemKey, .. bpModelDictionary[modelItemKey]];
+
+					XferObject.FullBpTree.tree[PanelBpName].Add(tempList);
+				}
+			}
+
+		}
+	}
+
+	public (Frame, int, int) GenerateNewBpTemplatePanelFromListModel(string PanelBpUniqueName)
+	{
+		int panelWidth = 300;
+		int panelHeight = 36;
+		//Entry TempVMEntryForLoop;
+
+		Frame bpFrameContainer = new Frame{BackgroundColor = Color.FromRgba("#EEEEEEFF"), //Overall Frame container
+										Margin = new Thickness(0,2),
+										Padding = new Thickness(0,0),
+										CornerRadius = 0
+										};
+		AbsoluteLayout bpFrameContainerAbsLayout = [];
+		
+		
+		/*BoxView bpNode = new BoxView{BackgroundColor = Color.FromRgba("#BBBBBBFF"),
+						Margin = new Thickness (0,0),
+						CornerRadius = 8,
+						};
+		AbsoluteLayout.SetLayoutBounds(bpNode, new Rect(panelWidth-20,28,16,16));
+		*/
+		Frame bpFrameVisualMain = new Frame{BackgroundColor = Color.FromRgba("#EEEEEEFF"), //Visual Frame container
+										Margin = new Thickness(0,2),
+										Padding = new Thickness(0,0),
+										CornerRadius = 3
+										};
+	
+		VerticalStackLayout bpFrameVisualMainVertLayout = [];
+		// XferObject.FullBpTree.tree[PanelBpUniqueName].Add(tempList);
+		//[0] = ["Categories", ..], [1] = ["buttonName", .. ], [2] = ["title", .. ]
+		Console.WriteLine("buttonName"+XferObject.FullBpTree.tree[PanelBpUniqueName]["buttonName"][0]); //Static issues, works when parent method is not static
+
+		/*
+		Label bpframeBtnName = new Label{Text = $": {XferObject.FullBpTree.tree[PanelBpUniqueName]["buttonName"][0]} :", 
+										FontSize = 12, 
+										Margin =  new Thickness(0,0), 
+										HorizontalTextAlignment = TextAlignment.Center,
+										VerticalTextAlignment = TextAlignment.Start
+									};
+		Label bpframeTitle = new Label{Text = $"{XferObject.FullBpTree.tree[PanelBpUniqueName]["title"][4]}", 
+										FontSize = 10, 
+										Margin =  new Thickness(0,0), 
+										HorizontalTextAlignment = TextAlignment.Start,
+										VerticalTextAlignment = TextAlignment.Start
+									};
+		
+		bpFrameVisualMainVertLayout.Add(bpframeBtnName);
+		bpFrameVisualMainVertLayout.Add(bpframeTitle);	
+		*/
+		int ItemCount = XferObject.FullBpTree.tree[PanelBpUniqueName].Count;
+		Grid panelLayoutGrid = new Grid{
+			 RowDefinitions = new RowDefinitionCollection()
+		};
+		RowDefinition rowDef = new RowDefinition{ Height = new GridLength(18, GridUnitType.Absolute) }; //18 = Node rect height + 2
+		//rowDef.Height = 12;
+		
+		ColumnDefinition ColOne = new ColumnDefinition();
+		ColumnDefinition ColTwo = new ColumnDefinition();
+		ColumnDefinition ColThree = new ColumnDefinition();
+		ColOne.Width = 180;
+		ColTwo.Width = 100;
+		ColThree.Width = 20;
+		
+		for (int i = 2; i < ItemCount; i++)
+		{
+			panelLayoutGrid.RowDefinitions.Add(rowDef);
+		}
+		panelLayoutGrid.ColumnDefinitions.Add(ColOne);
+		panelLayoutGrid.ColumnDefinitions.Add(ColTwo);
+		panelLayoutGrid.ColumnDefinitions.Add(ColThree);
+
+		int PanelLayoutGridRow = 0;
+		OrderedDictionary EntryListItem = XferObject.FullBpTree.tree[PanelBpUniqueName];
+		IDictionaryEnumerator EntryListEnum = EntryListItem.GetEnumerator();
+		EntryListEnum.MoveNext();
+		EntryListEnum.MoveNext();
+		EntryListEnum.MoveNext();
+		//for (int i = 3; i < ItemCount; i++)
+		while (EntryListEnum.MoveNext())
+		{
+			//List<string> AnotherCopyIdx = EntryListItem[i] as List<string>;
+			//string EntryItemKey = EntryListItem[i].Last();
+			List<string> AnotherCopyIdx = EntryListEnum.Value as List<string>;
+			string EntryItemKey = EntryListEnum.Key as string;
+			//Console.WriteLine("AnotherCopyIdx[0]:"+AnotherCopyIdx[0]);
+			Console.WriteLine("AnotherCopyIdx[0]:"+EntryItemKey);
+			BoxView bpNode = new BoxView{};
+			var TempVMLabelForLoop = new Label{
+											Text = EntryItemKey,
+											//Text = AnotherCopyIdx.Last(),
+											FontSize = 10, 
+											Margin =  new Thickness(0,0), 
+											HorizontalTextAlignment = TextAlignment.Start,
+											VerticalTextAlignment = TextAlignment.Start
+											};
+			if (AnotherCopyIdx[2] == "node"){
+				//BoxView bpNode = new BoxView{
+						bpNode.BackgroundColor = Color.FromRgba("#CCBBBBFF");
+						bpNode.Margin = new Thickness (0,0);
+						bpNode.WidthRequest = 16;
+						bpNode.HeightRequest = 16;
+						bpNode.CornerRadius = 8;
+						bpNode.ZIndex = 100;
+						//bpNode.VerticalOptions = Rect;
+						//};
+				//AbsoluteLayout.SetLayoutBounds(bpNode, new Rect(panelWidth-20,panelHeight-17,16,16));
+				//Grid.SetRow(bpNode,PanelLayoutGridRow);
+				//Grid.SetColumn(bpNode,2);
+				//panelLayoutGrid.Children.Add(bpNode);
+			}
+
+			Grid.SetRow(TempVMLabelForLoop,PanelLayoutGridRow);
+			Grid.SetColumn(TempVMLabelForLoop,0);
+			panelLayoutGrid.Children.Add(TempVMLabelForLoop);
+			AbsoluteLayout.SetLayoutBounds(bpNode, new Rect(panelWidth-18,panelHeight-17,16,16));
+			if (AnotherCopyIdx[2] == "node"){bpFrameContainerAbsLayout.Add(bpNode);}
+			PanelLayoutGridRow++;
+			panelHeight = panelHeight+18;
+			
+		}
+		AbsoluteLayout.SetLayoutBounds(bpFrameVisualMain, new Rect(10,0,panelWidth-20, panelHeight-4)); //Set width, height of Visual Container based on Generated Content
+
+		bpFrameVisualMainVertLayout.Add(panelLayoutGrid);
+		bpFrameVisualMain.Content = bpFrameVisualMainVertLayout;
+
+		bpFrameContainerAbsLayout.Add(bpFrameVisualMain);
+		//bpFrameContainerAbsLayout.Add(bpNode);
+		bpFrameContainer.Content = bpFrameContainerAbsLayout;
+
+		return (bpFrameContainer, panelHeight, panelWidth);
+	}
+	public (string, Frame) GenerateNewBpTemplatePanel(string bpModelKey) //New empty panel generator - pulls data fields from BlueprintModels 
 	{
 		//var dropData = "testData";
 		int testWidth = 200;
 		int testHeight = 400;
+		//string PanelBpName = $"${BlueprintModels.ModelDictionary[bpModelKey]["$type"]}${Guid.NewGuid()}";
+		string PanelBpNameBase = BlueprintModels.ModelDictionary[bpModelKey]["$type"];
+		string PanelBpName = $"${PanelBpNameBase}${Guid.NewGuid()}";
+
+
 		Frame bpFrameContainer = new Frame{BackgroundColor = Color.FromRgba("#EEEEEEFF"), //Overall Frame container
 										Margin = new Thickness(0,2),
 										Padding = new Thickness(0,0),
@@ -157,6 +409,7 @@ public partial class TreePage : ContentPage
 										HorizontalTextAlignment = TextAlignment.Start,
 										VerticalTextAlignment = TextAlignment.Start
 									};
+
 		bpFrameVisualMainVertLayout.Add(bpframeLabel);		
 		bpFrameVisualMainVertLayout.Add(bpframeLineItem);
 		bpFrameVisualMain.Content = bpFrameVisualMainVertLayout;
@@ -164,7 +417,7 @@ public partial class TreePage : ContentPage
 		bpFrameContainerAbsLayout.Add(bpFrameVisualMain);
 		bpFrameContainerAbsLayout.Add(bpNode);
 		bpFrameContainer.Content = bpFrameContainerAbsLayout;
-		return bpFrameContainer;
+		return (PanelBpName, bpFrameContainer);
 		
 	}
 }
